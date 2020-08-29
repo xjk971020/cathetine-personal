155afafcfc898b99f5810031486bbd17c1fe1bd9
XJKnice <823840954@qq.com>
2020-08-29 23:26:08 +0800

    1、线程顺序打印测试类。

--- cathetine-personal-test/src/test/java/com/cathetine/aboutJDK/ThreadTest.java
+++ cathetine-personal-test/src/test/java/com/cathetine/aboutJDK/ThreadTest.java
@@ -8,13 +8,17 @@ import org.junit.Test;
  * @date 2020/8/29 -  15:16
  **/
 public class ThreadTest {
-
-    public static void main(String[] args) {
+    @Test
+    public void test() throws InterruptedException {
         OrderPrinter orderPrinter = new OrderPrinter(5);
         new Thread(() -> orderPrinter.print(1)).start();
         new Thread(() -> orderPrinter.print(2)).start();
         new Thread(() -> orderPrinter.print(3)).start();
         new Thread(() -> orderPrinter.print(4)).start();
         new Thread(() -> orderPrinter.print(5)).start();
+
+        //需要在主线程中加休眠时间，不然看不到效果，在junit中，主线程执行完成程序便会退出。
+        Thread.sleep(1000000000);
     }
+
 }
